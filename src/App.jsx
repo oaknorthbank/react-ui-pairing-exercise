2ed880d00efa2da48b57bc98b263fda47d8d0583
adamant-uk <adam.gibson@oaknorth.co.uk>
2022-11-15 15:16:20 +0000

    add basic loan applications data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import {useEffect, useState} from "react";
+import loanApplicationData from './loanApplications.json'
+
 const App = () => {
+  const [loanApplications, setLoanApplications] = useState()
+  useEffect(() => {
+    setLoanApplications(loanApplicationData.data)
+  }, [])
+
   return (
     <div className="App">
       <h1>hello world</h1>
+      <p>{loanApplications && loanApplications[0].clientName}</p>
     </div>
   );
 };
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,14 @@ import { describe, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "./App.jsx";
 
-describe("App tests", () => {
-  test("Should display hello world", () => {
+describe("Dashboard view", () => {
+  test("Should display hello world title", () => {
     render(<App />);
     screen.getByText("hello world");
+  });
+
+  test("Should display the first loan application data", () => {
+    render(<App />);
+    screen.getByText("Alice Jones");
   });
 });
