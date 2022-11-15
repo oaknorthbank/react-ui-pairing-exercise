4ff0ac603875c95475e4fc82da19a4b28680e536
adamant-uk <adam.gibson@oaknorth.co.uk>
2022-11-15 15:38:25 +0000

    remove should

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,13 +2,13 @@ import { describe, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "./App.jsx";
 
-describe("Dashboard view", () => {
-  test("Should display hello world title", () => {
+describe("Loan applications dashboard view", () => {
+  test("displays a hello world title", () => {
     render(<App />);
     screen.getByText("hello world");
   });
 
-  test("Should display the first loan application data", () => {
+  test("displays the first loan application data", () => {
     render(<App />);
     screen.getByText("Alice Jones");
   });
