08927f00f5b446edc2353b685359381d24c98f06
Snigdha.Kasturi <snigdha.kasturi@ON-LON-MAC048.local>
2023-01-06 11:27:09 +0000

    Add new tests and table

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,30 @@ const App = () => {
   return (
     <div className="App">
       <h1>hello world</h1>
-      <p>{loanApplications && loanApplications[0].clientName}</p>
+      <table>
+        <tr>
+          <th>Client Details</th>
+          <th>Address</th>
+          <th>Type</th>
+          <th>All in rate</th>
+          <th>Loan amount</th>
+          <th>Created on</th>
+          <th>Status</th>
+        </tr>
+        {loanApplications &&
+          loanApplications.map((application) => (
+          <tr>
+            <td>{application.clientName}</td>
+            <td>{application.propertyAddress}</td>
+            <td>{application.loanType}</td>
+            <td>{application.allInRate}</td>
+            <td>{application.loanAmount}</td>
+            <td>{application.createdAt}</td>
+            <td>{(application.submittedAt) ? "Submitted" : "Incomplete"}</td>
+          </tr>
+          ))
+      }
+      </table>
     </div>
   );
 };
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,8 +8,36 @@ describe("Loan applications dashboard view", () => {
     screen.getByText("hello world");
   });
 
-  test("displays the first loan application data", () => {
+  test("displays header of the Loan Application Dashboard", () => {
+    render(<App />);
+    screen.getByText("Client Details");
+    screen.getByText("Address");
+    screen.getByText("Type");
+    screen.getByText("All in rate");
+    screen.getByText("Loan amount");
+    screen.getByText("Created on");
+    screen.getByText("Status");
+  });
+
+  test("displays details of first application", () => {
     render(<App />);
     screen.getByText("Alice Jones");
+    screen.getByText("17 Fore St, Somerset, TA21 8AA");
+    screen.getAllByText("RESIDENTIAL");
+    screen.getAllByText("0.072");
+    screen.getAllByText("500000");
+    screen.getByText("2022-11-15T10:29:22.508871");
+    screen.getAllByText("Incomplete");
+  });
+
+  test("displays details of second application", () => {
+    render(<App />);
+    screen.getByText("Daffy Duck");
+    screen.getByText("65 Rowlands Rd, West Sussex, BN11 3JN");
+    screen.getByText("COMMERCIAL");
+    screen.getByText("0.054");
+    screen.getByText("870000");
+    screen.getByText("2022-11-15T09:27:27.986370");
+    screen.getByText("Submitted");
   });
 });
